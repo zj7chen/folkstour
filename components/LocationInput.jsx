0e6e80fd79017f3a473dec33c1dc8008f5e5a559
Zhaojie Chen <zj.chansk@gmail.com>
2021-03-29 01:04:38 -0400

    Used React Select for location input

--- components/LocationInput.jsx
+++ components/LocationInput.jsx
@@ -1,11 +1,7 @@
-import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import styles from "./LocationInput.module.css";
 import countries from "cities.json";
 import Fuse from "fuse.js";
-import { useEffect } from "react";
-import Dropdown from "react-bootstrap/Dropdown";
-import { forwardRef } from "react";
+import React, { useState } from "react";
+import AsyncSelect from "react-select/async";
 
 const cityList = [];
 for (const [country, provinces] of Object.entries(countries)) {
@@ -20,66 +16,42 @@ const fuse = new Fuse(cityList, {
   keys: ["country", "province", "city"],
 });
 
-const QueryInput = forwardRef((props, ref) => {
-  return (
-    <Form.Control
-      ref={ref}
-      value={props.value}
-      placeholder={props.placeholder}
-      onChange={props.onChange}
-      onClick={props.onClick}
-    />
-  );
-});
-
 function locationToString(location) {
   if (!location) return "";
   return `${location.city}, ${location.province}, ${location.country}`;
 }
 
 function LocationInput(props) {
-  const [query, setQuery] = useState("");
-  const [result, setResult] = useState([]);
-  const [show, setShow] = useState(false);
-
-  useEffect(() => {
-    console.log("schedule");
-    const handle = setTimeout(() => {
-      console.log("exec");
-      const result = fuse.search(query, { limit: 4 });
-      setResult(result);
-    }, 200);
-    return () => clearTimeout(handle);
-  }, [query]);
+  const [value, setValue] = useState(null);
+  const [handle, setHandle] = useState();
+  console.log(value);
 
   return (
-    <Dropdown
-      show={show && result.length > 0}
-      onToggle={(isOpen, event, metadata) => {
-        console.log(isOpen, event, metadata);
-        setShow(isOpen);
+    <AsyncSelect
+      id={props.field.id}
+      name={props.field.name}
+      value={props.form.value}
+      onChange={({ value }) =>
+        props.form.setFieldValue(props.field.name, value)
+      }
+      placeholder={props.placeholder}
+      cacheOptions
+      loadOptions={(inputValue, callback) => {
+        clearTimeout(handle);
+        setHandle(
+          setTimeout(() => {
+            const result = fuse.search(inputValue, { limit: 4 });
+            callback(
+              result.map(({ item }) => ({
+                value: item,
+                label: locationToString(item),
+              }))
+            );
+          }, 300)
+        );
       }}
-    >
-      <Dropdown.Toggle
-        as={QueryInput}
-        value={show ? query : locationToString(props.field.value)}
-        placeholder={props.placeholder}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <Dropdown.Menu>
-        {result.map(({ item, refIndex }) => (
-          <Dropdown.Item
-            key={refIndex}
-            onClick={() => {
-              setQuery(locationToString(item));
-              props.form.setFieldValue(props.field.name, item);
-            }}
-          >
-            {locationToString(item)}
-          </Dropdown.Item>
-        ))}
-      </Dropdown.Menu>
-    </Dropdown>
+      defaultOptions
+    />
   );
 }
 
