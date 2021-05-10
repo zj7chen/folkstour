a97520fb994b8ec429e7886bee028b43b6d6a16f
Zhaojie Chen <zj.chansk@gmail.com>
2021-05-10 00:34:52 -0400

    Add background image to LoginPage

--- pages/login.jsx
+++ pages/login.jsx
@@ -6,6 +6,7 @@ import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
+import styles from "./login.module.css";
 
 function LoginPage() {
   // useEffect(() => {
@@ -18,147 +19,159 @@ function LoginPage() {
   const router = useRouter();
   const { redirect } = router.query;
   return (
-    <Modal.Dialog>
-      <Formik
-        initialValues={{
-          email: "",
-          password: "",
-          confirmPass: "",
-          name: "",
-          gender: "",
-          signingUp: false,
-        }}
-        onSubmit={async ({ signingUp, email, password, name, gender }) => {
-          await submit(signingUp ? "/api/signup" : "/api/login", {
-            email,
-            password,
-            name,
-            gender,
-          });
-          router.replace(redirect ?? "/search-trip");
-        }}
-      >
-        {({
-          handleSubmit,
-          handleChange,
-          handleBlur,
-          setFieldValue,
-          values,
-          touched,
-          isValid,
-          errors,
-        }) => (
-          <Form onSubmit={handleSubmit}>
-            <Modal.Header>
-              <Modal.Title>
-                {values.signingUp ? "Sign up" : "Login"}
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Form.Group controlId="email">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  value={values.email}
-                  onChange={handleChange}
-                  type="email"
-                  placeholder="Enter email"
-                />
-              </Form.Group>
+    <div className={styles.background}>
+      <div className={styles.modal}>
+        <Modal.Dialog
+          className={styles.dialog}
+          contentClassName={styles.dialogContent}
+        >
+          <Formik
+            initialValues={{
+              email: "",
+              password: "",
+              confirmPass: "",
+              name: "",
+              gender: "",
+              signingUp: false,
+            }}
+            onSubmit={async ({ signingUp, email, password, name, gender }) => {
+              await submit(signingUp ? "/api/signup" : "/api/login", {
+                email,
+                password,
+                name,
+                gender,
+              });
+              router.replace(redirect ?? "/search-trip");
+            }}
+          >
+            {({
+              handleSubmit,
+              handleChange,
+              handleBlur,
+              setFieldValue,
+              values,
+              touched,
+              isValid,
+              errors,
+            }) => (
+              <Form onSubmit={handleSubmit}>
+                <Modal.Header>
+                  <Modal.Title>
+                    {values.signingUp ? "Sign up" : "Login"}
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Form.Group controlId="email">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                      value={values.email}
+                      onChange={handleChange}
+                      type="email"
+                      placeholder="Enter email"
+                    />
+                  </Form.Group>
 
-              <Form.Group controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  value={values.password}
-                  onChange={handleChange}
-                  type="password"
-                  placeholder="Password"
-                />
-              </Form.Group>
+                  <Form.Group controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      value={values.password}
+                      onChange={handleChange}
+                      type="password"
+                      placeholder="Password"
+                    />
+                  </Form.Group>
 
-              {values.signingUp && (
-                <Form.Group controlId="confirmPass">
-                  <Form.Label>Confirm Password</Form.Label>
-                  <Form.Control
-                    value={values.confirmPass}
-                    onChange={handleChange}
-                    type="password"
-                    placeholder="Confirm Password"
-                  />
-                </Form.Group>
-              )}
-
-              {values.signingUp && (
-                <Form.Group controlId="name">
-                  <Form.Label>Name</Form.Label>
-                  <Form.Control
-                    value={values.name}
-                    onChange={handleChange}
-                    type="text"
-                    placeholder="What would you like to be called?"
-                  />
-                </Form.Group>
-              )}
-
-              {values.signingUp && (
-                <Form.Group controlId="gender">
-                  <Form.Label>Gender</Form.Label>
-                  <div>
-                    {Object.entries(GENDERS).map(([key, { displayText }]) => (
-                      <Form.Check
-                        key={key}
-                        id={`gender-${key}`}
-                        inline
-                        type="radio"
-                        label={displayText}
-                        name="gender"
-                        value={key}
+                  {values.signingUp && (
+                    <Form.Group controlId="confirmPass">
+                      <Form.Label>Confirm Password</Form.Label>
+                      <Form.Control
+                        value={values.confirmPass}
                         onChange={handleChange}
-                        checked={values.gender === key}
+                        type="password"
+                        placeholder="Confirm Password"
                       />
-                    ))}
-                  </div>
-                  <Form.Text className="text-muted">
-                    Gender will affect you when trips have gender requirements
-                  </Form.Text>
-                </Form.Group>
-              )}
+                    </Form.Group>
+                  )}
 
-              {errors.mismatch && (
-                <Alert variant="danger">{errors.mismatch}</Alert>
-              )}
+                  {values.signingUp && (
+                    <Form.Group controlId="name">
+                      <Form.Label>Name</Form.Label>
+                      <Form.Control
+                        value={values.name}
+                        onChange={handleChange}
+                        type="text"
+                        placeholder="What would you like to be called?"
+                      />
+                    </Form.Group>
+                  )}
 
-              {!values.signingUp && (
-                <Form.Group controlId="remember">
-                  <Form.Check type="checkbox" label="Remember me" />
-                </Form.Group>
-              )}
+                  {values.signingUp && (
+                    <Form.Group controlId="gender">
+                      <Form.Label>Gender</Form.Label>
+                      <div>
+                        {Object.entries(GENDERS).map(
+                          ([key, { displayText }]) => (
+                            <Form.Check
+                              key={key}
+                              id={`gender-${key}`}
+                              inline
+                              type="radio"
+                              label={displayText}
+                              name="gender"
+                              value={key}
+                              onChange={handleChange}
+                              checked={values.gender === key}
+                            />
+                          )
+                        )}
+                      </div>
+                      <Form.Text className="text-muted">
+                        Gender will affect you when trips have gender
+                        requirements
+                      </Form.Text>
+                    </Form.Group>
+                  )}
 
-              {values.signingUp && (
-                <Form.Group controlId="agree">
-                  <Form.Check
-                    type="checkbox"
-                    label="Agree to terms and conditions"
-                  />
-                </Form.Group>
-              )}
-            </Modal.Body>
-            <Modal.Footer>
-              <Button
-                variant="link"
-                onClick={() => setFieldValue("signingUp", !values.signingUp)}
-              >
-                {values.signingUp
-                  ? "Already have an account?"
-                  : "Create an account"}
-              </Button>
-              <Button type="submit" variant="primary">
-                {values.signingUp ? "Sign up" : "Login"}
-              </Button>
-            </Modal.Footer>
-          </Form>
-        )}
-      </Formik>
-    </Modal.Dialog>
+                  {errors.mismatch && (
+                    <Alert variant="danger">{errors.mismatch}</Alert>
+                  )}
+
+                  {!values.signingUp && (
+                    <Form.Group controlId="remember">
+                      <Form.Check type="checkbox" label="Remember me" />
+                    </Form.Group>
+                  )}
+
+                  {values.signingUp && (
+                    <Form.Group controlId="agree">
+                      <Form.Check
+                        type="checkbox"
+                        label="Agree to terms and conditions"
+                      />
+                    </Form.Group>
+                  )}
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button
+                    variant="link"
+                    onClick={() =>
+                      setFieldValue("signingUp", !values.signingUp)
+                    }
+                  >
+                    {values.signingUp
+                      ? "Already have an account?"
+                      : "Create an account"}
+                  </Button>
+                  <Button type="submit" variant="primary">
+                    {values.signingUp ? "Sign up" : "Login"}
+                  </Button>
+                </Modal.Footer>
+              </Form>
+            )}
+          </Formik>
+        </Modal.Dialog>
+      </div>
+    </div>
   );
 }
 
