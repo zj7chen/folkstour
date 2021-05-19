827b2c981565d63979c079cba515a4898b873ac8
Zhaojie Chen <zj.chansk@gmail.com>
2021-05-18 20:40:48 -0400

    Add comma to date format

--- client/display.js
+++ client/display.js
@@ -4,6 +4,13 @@ export function displayLocation(location) {
   return `${location.city}, ${location.province}, ${location.country}`;
 }
 
+const dateFormat = new Intl.DateTimeFormat("en", {
+  weekday: "short",
+  month: "short",
+  day: "2-digit",
+  year: "numeric",
+});
+
 export function displayDate(date) {
-  return date.toDateString();
+  return dateFormat.format(date);
 }
